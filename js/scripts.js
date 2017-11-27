7d96d7617225d0a988857f0a4867518415190736
Julita Lutostańska <julita.lutostanska@gmail.com>
2017-11-27 17:34:55 +0100

    Change scripts.js content

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var a = 3,
+	h = 4;
+var triangleArea;
+
+a = prompt ('enter triangle base', a);
+h = prompt ('enter triangle height', h);
+
+triangleArea = a * h / 2;
+
+alert('Triangle surface area is equal to: ' + triangleArea);
+console.log('Triangle surface area with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
